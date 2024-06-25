7d2f688b059c2083a2c0caf951ff18aecf055ac8
CyrilAshong <ashongcyril2704@gmail.com>
2024-06-25 12:43:58 +0000

    Created the bottom tab navigation for the four screens

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import HomePage from "./components/HomePage";
+import MyCardsPage from "./components/MyCardsPage";
+import StatisticsPage from "./components/StatisticsPage";
+import SettingsPage from "./components/SettingsPage";
+
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomePage}/>
+        <Tab.Screen name="MyCards" component={MyCardsPage}/>
+        <Tab.Screen name="Statistics" component={StatisticsPage}/>
+        <Tab.Screen name="Settings" component={SettingsPage}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
